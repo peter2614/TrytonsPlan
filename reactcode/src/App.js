caef6b6857ab03cb551a7d458593932ae87dfeea
Alex Monji <ammonji@ucsd.edu>
2018-05-12 00:13:15 -0700

    First MainPage Branch Commit.
    Signed-off-by: Alex Monji <ammonji@ucsd.edu>

--- reactcode/src/App.js
+++ reactcode/src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MainPage from './Main/MainPage.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+        <div>
+            <MainPage/>
+
+
+        </div>
+      /*
+        <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
@@ -13,7 +20,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
+      </div>*/
     );
   }
 }
--- reactcode/src/Main/MainPage.js
+++ reactcode/src/Main/MainPage.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
+import CourseSearchBar from "./Components/CourseSearchBar";
+
 
 class MainPage extends Component {
+    constructor(props) {
+        super(props);
+
+
+    }
+
     render() {
-        return
+        return (
+            <div><CourseSearchBar/></div>
+
+        )
 
     }
 
 
 
 }
+
+export default MainPage;
