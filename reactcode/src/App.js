eef90aaf75ae3af8fe295a1c92f8fe9d29fded75
Alex Monji <ammonji@ucsd.edu>
2018-05-17 22:50:28 -0700

    Overhauled the react code. Now it's more organized, has a search bar and course list. Run npm start under reactcode to see what's been changed.
    Signed-off-by: Alex Monji <ammonji@ucsd.edu>

--- reactcode/src/App.js
+++ reactcode/src/App.js
@@ -1,26 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import MainPage from './Main/MainPage.js';
+import MainPage from './Pages/Main/MainPage';
 
 class App extends Component {
   render() {
+
     return (
-        <div>
-            <MainPage/>
-
-
-        </div>
-      /*
-        <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>*/
+      <div className="App">
+        <MainPage />
+      </div>
     );
   }
 }
